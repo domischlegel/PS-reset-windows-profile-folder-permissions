b00f363d7dfef605b97134e1da23a990f7af68fa
PhaNDoMs <phandoms.business@gmail.com>
2018-11-02 12:49:35 +0100

    changed variables to english

--- ResetWindowsProfileFolderPermissions.ps1
+++ ResetWindowsProfileFolderPermissions.ps1
@@ -3,10 +3,10 @@ Start-Transcript -path "D:\ResetWindowsProfileFolderPermissions\output.log"
 
 # Change this to the Path of the Profile Folder
 Set-Location "D:\Profiles"
-$Pfad = "D:\Profiles"
+$Path = "D:\Profiles"
 
 # Get all Child folders
-$Folders = Get-ChildItem -Path $Pfad
+$Folders = Get-ChildItem -Path $Path
 
 # for every folder 
 ForEach ($Folder in $Folders){
@@ -22,7 +22,7 @@ $Dir = $Folder.Name
 Write-Host "`n`n`nDoing $User"
 
 # Get Permissions
-$ACL = Get-Acl "$Pfad\$Dir"
+$ACL = Get-Acl "$Path\$Dir"
 
 # Remove Inherit from above
 $acl.SetAccessRuleProtection($True, $True)
@@ -32,9 +32,9 @@ $ACL.Access | %{$acl.RemoveAccessRule($_)}
 
 # Set new Permissions
 $ACL.AddAccessRule((New-Object System.Security.AccessControl.FileSystemAccessRule("$User", "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")))
-$ACL.AddAccessRule((New-Object System.Security.AccessControl.FileSystemAccessRule("Administratoren", "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")))
+$ACL.AddAccessRule((New-Object System.Security.AccessControl.FileSystemAccessRule("Administrators", "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")))
 $ACL.AddAccessRule((New-Object System.Security.AccessControl.FileSystemAccessRule("System", "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")))
-Set-Acl "$Pfad\$Dir" $Acl
+Set-Acl "$Path\$Dir" $Acl
 Write-Host "$User done"
 }
 
